10c68a0dacebbf592b42992b76bd84f7343d303b
Daniel Chukwuji <chukwujidan@gmail.com>
2025-09-25 15:09:11 +0100

    Initial commit: setup Django project

--- investor/settings_pro.py
+++ investor/settings_pro.py
@@ -3,10 +3,8 @@ import os
 from .settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = os.environ.get(
-    "DJANGO_ALLOWED_HOSTS",
-    "cryptotradeglobe.com,www.cryptotradeglobe.com,16.171.140.202,127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = ["cryptotradeglobe.com", "www.cryptotradeglobe.com", "16.171.140.202"]
+
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.zoho.com'
@@ -17,9 +15,9 @@ EMAIL_HOST_USER = 'info@cryptotradeglobe.com'
 EMAIL_HOST_PASSWORD = 'D@ntech1212'
 
 # Security
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 CORS_ALLOWED_ORIGINS = [
